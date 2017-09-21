d95fc0a70a4001a3167dd5692882da03dd68c215
Your Name <avimarathe@avinashs-MacBook-Pro.local>
2017-09-21 17:20:54 +0530

    first message

---
+++ MCart/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  MCart
+//
+//  Created by avi marathe on 21/09/17.
+//  Copyright © 2017 avi marathe. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+let To_REAR = "to_rear"
